b661f0079ed065408bbe1e8d1e9e7aea3717c365
Paweł Wiejacha <pawel.wiejacha@gmail.com>
2015-05-13 00:23:25 +0200

    core: added Upper Confidence Bounds computation

--- core/src/main/scala/edison/search/Samples.scala
+++ core/src/main/scala/edison/search/Samples.scala
@@ -20,6 +20,19 @@ case class Samples(values: List[Sample]) {
   def max: Option[Result] = results.reduceOption(_ max _)
   def min: Option[Result] = results.reduceOption(_ min _)
 
+  /**
+   * Computes the Upper Confidence Bound.
+   *
+   * @param alpha tunable bias (exploitation vs exploration) parameter
+   * @param parentVisitNum number of times the parent node was visited
+   */
+  def ucb(alpha: Double, parentVisitNum: Int): Option[Double] = {
+    mean map { m =>
+      // \mu_i  + \alpha * (ln(N) / n_i)^(1/2)
+      m + alpha * Math.sqrt(Math.log(parentVisitNum) / size)
+    }
+  }
+
   def variance: Option[Result] = {
     mean map { m =>
       // VX = E(X^2) - EX^2
---
+++ core/src/test/scala/edison/search/UcbTest.scala
@@ -0,0 +1,63 @@
+package edison.search
+
+import edison.util.SmartSpec
+import org.scalactic.TolerantNumerics
+
+class UcbTest extends SmartSpec {
+
+  val alpha = 2
+  val countsToTest = List(1, 2, 3, 4, 10, 100, 1000, 10000)
+
+  implicit val doubleEquality = TolerantNumerics.tolerantDoubleEquality(0.001)
+
+  implicit class UcbHelper(samples: Samples) {
+    def ucb(parentVisitNum: Int): Double = samples.ucb(alpha, parentVisitNum).get
+  }
+
+  def createSamples(mean: Result, count: Int) = Samples(List.fill(count)(Sample(1, mean)))
+
+  behavior of "Samples.ucb"
+
+  it must "return None for an empty samples" in {
+    Samples.empty.ucb(alpha, 5) shouldBe None
+  }
+
+  it must "favor less explored node, when both means are equal" in {
+    for (count <- countsToTest) {
+      val lessExplored = createSamples(mean = 10.0, count = count)
+      val moreExplored = createSamples(mean = 10.0, count = count + 1)
+      val n = count * 2 + 1
+
+      lessExplored.mean shouldBe moreExplored.mean
+      lessExplored.ucb(n) shouldBe >(moreExplored.ucb(n))
+    }
+  }
+
+  it must "favor a node with better mean, when both nodes are equally explored" in {
+    for (count <- countsToTest) {
+      val better = createSamples(mean = 10.0, count = count)
+      val worse = createSamples(mean = 9.9, count = count)
+      val n = count * 2
+
+      better.ucb(n) shouldBe >(worse.ucb(n))
+    }
+  }
+
+  it must "converge to mean" in {
+    val ucbs = countsToTest map { count => createSamples(mean = 10.0, count = count).ucb(count * 5) }
+
+    ucbs.reverse should be(sorted)
+    ucbs.head shouldBe >(ucbs.last)
+    ucbs.head shouldBe >(11.0)
+    (ucbs.last - 10.0) shouldBe <(0.1)
+  }
+
+  it must "re-explore ignored nodes" in {
+    val explored = createSamples(mean = 10.0, count = 1000)
+    val ignored = createSamples(mean = 9.0, count = 10)
+
+    explored.ucb(1000) shouldBe >(ignored.ucb(10))
+    explored.ucb(1000) shouldBe <(ignored.ucb(1000))
+  }
+
+}
